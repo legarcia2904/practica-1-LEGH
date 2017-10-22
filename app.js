20778d6a1f3d7d7be8693bb76d4d34357457b6e1
legarcia2904 <legarcia2904@gmail.com>
2017-10-22 17:11:32 -0400

    actualizacion de archivos
    actualizacion de archivos para usar exactamente las dependencias del proyecto inicial

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var express = require('express');
-//var path = require('path');
-//var favicon = require('serve-favicon');
+var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -12,22 +11,19 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', index);
-//app.use('/users', users);
-require('./routes/index')(app);
+app.use('/', index);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-//var multer = require('multer'); // v1.0.5
-//var upload = multer(); // for parsing multipart/form-data
+var multer = require('multer'); // v1.0.5
+var upload = multer(); // for parsing multipart/form-data
 
 var LibrosCtrl = require('../controllers/LibrosCtrl');
 
@@ -19,14 +19,23 @@ router.post('/libros', function(req,res,next){
 	res.status(200).jsonp({nombre:'código Da Vinci', autor:'Dan Brown'});
 });*/
 
-/*router.route('/libros')
+router.route('/libros')
 	.get(LibrosCtrl.getLibros)
 	.post(upload.array(),LibrosCtrl.addLibro);
 
 router.route('/libros/:id')
 	.get(LibrosCtrl.getById)
-	.put(upload.array(), LibrosCtrl.updateLibro)
-	.delete(LibrosCtrl.deleteLibro);*/
+	.put(upload.array(),LibrosCtrl.updateLibro)
+	.delete(upload.array(),LibrosCtrl.deleteLibro);
+
+router.route('/autores')
+	.get(LibrosCtrl.getAutores);
+
+router.route('/autores/:nombre')
+	.get(LibrosCtrl.getAutoresByNombre)
+	.put(upload.array(),LibrosCtrl.updateAutores)
+	.delete(upload.array(),LibrosCtrl.deleteAutor);
+
 /*
 router.route('/autores')
 	.get(); //Devolver todos los autores
@@ -38,17 +47,4 @@ router.route('/autores/:nombre')
 */
 	//Utilizar git, crear una cuenta en github y subir tarea1; no subir los node_modules; hacer README.md
 
-//module.exports = router;
-
-module.exports = function (app) {
-    app.get('/libros', LibrosCtrl.getLibros);
-    app.get('/libros/:id', LibrosCtrl.getById);
-    app.post('/libros', LibrosCtrl.addLibro);
-    app.put('/libros/:id', LibrosCtrl.updateLibro);
-    app.delete('/libros/:id', LibrosCtrl.deleteLibro);
-
-    app.get('/autores', LibrosCtrl.getAutores);
-    app.get('/autores/:nombre', LibrosCtrl.getAutoresByNombre);
-    app.put('/autores/:nombre', LibrosCtrl.updateAutores);
-    app.delete('/autores/:nombre', LibrosCtrl.deleteAutor);
-};
+module.exports = router;
